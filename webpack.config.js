ce6ac5c75a4b77898e2db4f1dc4bd77a906103e5
hsin421 <shinsonh@gmail.com>
2016-09-30 17:47:16 -0400

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ var path = require('path');
 
 module.exports = {
 	entry: [
-		'webpack/hot/dev-server',
-		'webpack-dev-server/client?http://localhost:8080',
 		path.resolve(__dirname, 'app/main.js')
 	],
 	output: {
